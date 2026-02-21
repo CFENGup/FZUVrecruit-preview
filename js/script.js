b59a428cc5ce154bc7d98b06e72e8c25c11e5f62
CFENG <131766671+CFENGup@users.noreply.github.com>
2026-02-22 02:21:01 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-alert("v0.22202 only preview   最后发布时间2.22 2:16");
+alert("v0.22202 preview only\n最后发布时间2.22 2:16");
 console.log("v0.22202 only preview   最后发布时间2.22 2:16");
 // ==================== 全局禁止文字选中/拖动（防蓝框） ====================
 document.addEventListener('selectstart', (e) => e.preventDefault());
